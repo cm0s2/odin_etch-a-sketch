9198f1827cfff91f9c42297115d73e504a285100
Carl Gudmundsson <komoss@gmail.com>
2022-01-19 14:21:22 +0100

    Finish basic functionality

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 const DRAW_AREA_SIZE = 480;
-let pixels = 3;
+
+const canvas = document.querySelector('#canvas');
+canvas.style.width = DRAW_AREA_SIZE + 'px';
+canvas.style.height = DRAW_AREA_SIZE + 'px';
 
 
-const container = document.querySelector('#container');
-container.style.width = DRAW_AREA_SIZE + 'px';
-container.style.height = DRAW_AREA_SIZE + 'px';
-
-for (let i = 0; i < pixels**2; i++) {
-    const pixel = document.createElement('div');
-    pixel.classList.add('pixel');
-    pixel.style.width = DRAW_AREA_SIZE / pixels + 'px';
-    pixel.style.height = DRAW_AREA_SIZE / pixels + 'px';
-
-    pixel.addEventListener('mouseover', changeColor);
-
-    container.appendChild(pixel);
-    
-}
 
 function changeColor(e) {
-    console.log(e.target);
     e.target.style.backgroundColor = 'black';
 }
+
+const resetBtn = document.querySelector('#reset');
+resetBtn.addEventListener('click', createDrawingArea);
+
+function createDrawingArea(size) {
+    while (isNaN(size) || size === 0 || size === null) size = prompt('Size: ');
+
+    // Reset container
+    canvas.innerHTML = '';
+
+    // Draw pixels
+    for (let i = 0; i < size**2; i++) {
+        const pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        pixel.style.width = DRAW_AREA_SIZE / size + 'px';
+        pixel.style.height = DRAW_AREA_SIZE / size + 'px';
+    
+        pixel.addEventListener('mouseover', changeColor);
+    
+        canvas.appendChild(pixel);
+        
+    }
+}
+
+createDrawingArea(20);
